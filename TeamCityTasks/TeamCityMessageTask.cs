46b0d7aa50e3ef62001aa7417181e2eca1075315
Jozef Izso <jozef.izso@gmail.com>
2015-11-17 23:05:09 +0100

    Added prototype of the build log message.

---
+++ TeamCityTasks/TeamCityMessageTask.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+
+namespace TeamCityTasks
+{
+    public class TeamCityMessageTask : Task
+    {
+        public string Text { get; set; }
+
+        ////public string ErrorDetails { get; set; }
+
+        ////public string Status { get; set; }
+
+        public override bool Execute()
+        {
+            string messageFormat = "##teamcity[message text='{0}']";
+            var escapedValue = this.Text.TeamCityEscape();
+            string message = String.Format(messageFormat, escapedValue);
+
+            this.Log.LogMessage(MessageImportance.Normal, message);
+
+            return true;
+        }
+    }
+}
---
+++ TeamCityTasksTests/TeamCityMessageTaskTests.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using Microsoft.Build.Utilities;
+using NUnit.Framework;
+using TeamCityTasks;
+
+namespace TeamCityTasksTests
+{
+    [TestFixture]
+    public class TeamCityMessageTaskTests
+    {
+        [Test]
+        public void TeamCityMessageTask_Class_IsMsbuildTask()
+        {
+            var task = new TeamCityMessageTask();
+
+            Assert.IsInstanceOf<Task>(task);
+        }
+
+        [Test]
+        public void Execute_TextIsSet_OutputsCorrectMessage()
+        {
+            var expectedText = "My TeamCity message.";
+
+            var engine = new MockBuildEngine();
+            var task = new TeamCityMessageTask();
+            task.BuildEngine = engine;
+
+            task.Text = expectedText;
+
+            var actualResult = task.Execute();
+
+            Assert.True(actualResult);
+            CollectionAssert.Contains(engine.LoggedMessages, "##teamcity[message text='My TeamCity message.']");
+        }
+
+        [Test]
+        public void Execute_ValueWithSpecialCharactersIsSet_OutputsEscapedMessage()
+        {
+            var expectedText = "My special ' | \n characters.";
+
+            var engine = new MockBuildEngine();
+            var task = new TeamCityMessageTask();
+            task.BuildEngine = engine;
+
+            task.Text = expectedText;
+
+            var actualResult = task.Execute();
+
+            Assert.True(actualResult);
+            CollectionAssert.Contains(engine.LoggedMessages, "My special |' || |n characters.']");
+        }
+    }
+}
