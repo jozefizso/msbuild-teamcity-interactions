11f326341426315bb17fb82a7903615879a0cca6
Jozef Izso <jozef.izso@gmail.com>
2015-11-17 22:22:01 +0100

    Simple extension method for escaping TeamCity messages.

---
+++ TeamCityTasks/StringExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TeamCityTasks
+{
+    public static class StringExtensions
+    {
+        public static string TeamCityEscape(this string s)
+        {
+            return s;
+        }
+    }
+}
---
+++ TeamCityTasksTests/StringExtensionsTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using Microsoft.Build.Utilities;
+using NUnit.Framework;
+using TeamCityTasks;
+
+namespace TeamCityTasksTests
+{
+    [TestFixture]
+    [SuppressMessage("ReSharper", "InvokeAsExtensionMethod")]
+    public class StringExtensionsTests
+    {
+        [Test]
+        public void StringExtensions_SimpleText_ReturnsTheSameText()
+        {
+            const string expectedMessage = "simple text";
+
+            var actualMessage = StringExtensions.TeamCityEscape(expectedMessage);
+
+            Assert.AreEqual(expectedMessage, actualMessage);
+        }
+    }
+}
