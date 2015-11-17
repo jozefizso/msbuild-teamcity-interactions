62b188baae0ef3fa261de1b856433d6eb6c86601
Jozef Izso <jozef.izso@gmail.com>
2015-11-17 22:39:22 +0100

    Implemented TeamCity value escaping (without Unicode values escaping).

--- TeamCityTasks/StringExtensions.cs
+++ TeamCityTasks/StringExtensions.cs
@@ -7,9 +7,42 @@ namespace TeamCityTasks
 {
     public static class StringExtensions
     {
-        public static string TeamCityEscape(this string s)
+        /// <summary>
+        /// Escaped a value according TeamCity system messages rules.
+        /// </summary>
+        /// <see cref="https://confluence.jetbrains.com/display/TCD8/Build+Script+Interaction+with+TeamCity#BuildScriptInteractionwithTeamCity-servMsgsServiceMessages"/>
+        public static string TeamCityEscape(this string value)
         {
-            return s;
+            var sb = new StringBuilder(value.Length);
+            foreach (var c in value)
+            {
+                switch (c)
+                {
+                    case '|':
+                        sb.Append("||");
+                        break;
+                    case '\'':
+                        sb.Append("|'");
+                        break;
+                    case '\n':
+                        sb.Append("|n");
+                        break;
+                    case '\r':
+                        sb.Append("|r");
+                        break;
+                    case '[':
+                        sb.Append("|[");
+                        break;
+                    case ']':
+                        sb.Append("|]");
+                        break;
+                    default:
+                        sb.Append(c);
+                        break;
+                }
+            }
+
+            return sb.ToString();
         }
     }
 }
--- TeamCityTasksTests/StringExtensionsTests.cs
+++ TeamCityTasksTests/StringExtensionsTests.cs
@@ -19,5 +19,31 @@ namespace TeamCityTasksTests
 
             Assert.AreEqual(expectedMessage, actualMessage);
         }
+
+        [Test]
+        [TestCase("'", "|'")]
+        [TestCase("\n", "|n")]
+        [TestCase("\r", "|r")]
+        [TestCase("|", "||")]
+        [TestCase("[", "|[")]
+        [TestCase("]", "|]")]
+        public void StringExtensions_SingleSpecialSymbol_ReturnsEscapedString(string symbol, string expectedEscapedValue)
+        {
+            var actualEscapedValue = StringExtensions.TeamCityEscape(symbol);
+
+            Assert.AreEqual(expectedEscapedValue, actualEscapedValue);
+        }
+
+        [Test]
+        [TestCase("'\n\r|[]", "|'|n|r|||[|]")]
+        [TestCase("||", "||||")]
+        [TestCase("my value's", "my value|'s")]
+        [TestCase("[1,2,3]|[1,2,3]", "|[1,2,3|]|||[1,2,3|]")]
+        public void StringExtensions_MultipleSpecialSymbols_ReturnsEscapedString(string symbol, string expectedEscapedValue)
+        {
+            var actualEscapedValue = StringExtensions.TeamCityEscape(symbol);
+
+            Assert.AreEqual(expectedEscapedValue, actualEscapedValue);
+        }
     }
 }
