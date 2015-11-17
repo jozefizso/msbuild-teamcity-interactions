67132e12b2be727082601720974057b4595b19df
Jozef Izso <jozef.izso@gmail.com>
2015-11-17 21:18:50 +0100

    Added empty VS project

---
+++ TeamCityTasks/Properties/AssemblyInfo.cs
@@ -0,0 +1,18 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General information about an assembly
+[assembly: AssemblyTitle("TeamCity Interaction Tasks")]
+[assembly: AssemblyDescription("MSBuild tasks for interactions with TeamCity.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TeamCityTasks")]
+[assembly: AssemblyCopyright("Copyright © 2015 Jozef Izso, MIT License")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("92b0d42b-f39a-4b70-908d-139e00befbb0")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
