c23e6b183a8efc54eb31f7993c0b0847bdad42e8
Jozef Izso <jozef.izso@gmail.com>
2015-11-24 16:58:40 +0100

    Added task for generating publishArtifacts message.

---
+++ TeamCityTasks/TeamCityPublishArtifacts.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+
+namespace TeamCityTasks
+{
+    public class TeamCityPublishArtifacts : Task
+    {
+        [Required]
+        public string Path { get; set; }
+
+        public override bool Execute()
+        {
+            string messageFormat = "##teamcity[publishArtifacts '{0}']";
+            var escapedPath = this.Path.TeamCityEscape();
+            string message = String.Format(messageFormat, escapedPath);
+
+            this.Log.LogMessage(MessageImportance.Normal, message);
+
+            return true;
+        }
+    }
+}
---
+++ TeamCityTasksTests/TeamCityPublishArtifactsTests.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.Build.Utilities;
+using NUnit.Framework;
+using TeamCityTasks;
+using TeamCityTasksTests.IntegrationTests;
+
+namespace TeamCityTasksTests
+{
+    [TestFixture]
+    public class TeamCityPublishArtifactsTests
+    {
+        [Test]
+        public void TeamCityPublishArtifacts_Class_IsMsbuildTask()
+        {
+            var task = new TeamCityPublishArtifacts();
+
+            Assert.IsInstanceOf<Task>(task);
+        }
+
+        [Test]
+        public void Execute_PathIsSet_OutputsCorrectMessage()
+        {
+            var expectedPath = "bin\\artifact.zip";
+
+            var engine = new MockBuildEngine();
+            var task = new TeamCityPublishArtifacts();
+            task.BuildEngine = engine;
+
+            task.Path = expectedPath;
+
+            var actualResult = task.Execute();
+
+            Assert.True(actualResult);
+            CollectionAssert.Contains(engine.LoggedMessages, "##teamcity[publishArtifacts 'bin\\artifact.zip']");
+        }
+    }
+}
